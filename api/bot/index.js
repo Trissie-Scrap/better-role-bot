f9bfa6aa94d6ebf05f1e5d34f5fb9e1a666da703
Nightmarlin <lmiller81694@gmail.com>
2019-04-20 22:44:19 +0100

    Added message listening

--- api/bot/index.js
+++ api/bot/index.js
@@ -3,11 +3,21 @@ const config = require('config')
 const debug = require('debug')('brb:bot')
 
 const BOT_TOKEN = config.get('discord.botToken')
+const FRONTEND_URL = config.get('api.frontendUrl')
 
 const client = new Discord.Client()
 
 client.on('ready', () => {
   debug('bot readied')
+})
+
+client.on('message', (message) => {
+  if (message.mentions.members.has(client.user.id)) {
+    message.reply(`Hiya ${message.member.displayName}, edit your roles for ${message.guild.name} at ${FRONTEND_URL}/#/s/${message.guild.id}`)
+  }
+  if (message.content === 'BRB Ping Plz') {
+    message.reply(`Current Ping: ${client.ping}ms`)
+  }
 })
 
 client.login(BOT_TOKEN)
